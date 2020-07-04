ff48169671b6e31a1ccd874818a1e691127e7ade
Jacopo Bufalino <jacopo.bufalino@vaana.fi>
2020-07-04 10:05:40 +0200

    Remove unused files and create README.md

--- run-calico-and-istio-on-single-namespace.sh
+++
@@ -1,8 +0,0 @@
-source ./run-kubernetes-cluster.sh
-
-restartMinikube
-installCalico
-buildAndApplyInspector
-installIstio
-sleep 60
-kubectl exec -it $(kubectl get pod -l app=inspector -o jsonpath='{.items[0].metadata.name}') npm run start | tee result-calico-istio-single-namespace.txt
--- test.sh
+++
@@ -1,4 +0,0 @@
-kubectl exec -it $(kubectl get pod -l app=inspector -o jsonpath='{.items[0].metadata.name}') bash
-cd /tmp
-nmap -T4 -v 172.17.0.0/24 | tee nmap.txt
-kubectl cp inspector-deployment-695f784cc4-dbgqg:/tmp/nmap.txt ./oych.txt
