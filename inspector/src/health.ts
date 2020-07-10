db42b99a3feb96fa16348e652d04a3c269ac235f
Jacopo Bufalino <jacopo.bufalino@vaana.fi>
2020-07-10 16:14:12 +0200

    Update health endpoint

--- inspector/src/health.ts
+++ inspector/src/health.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 export const registerHealthCheck = (app: express.Application) => {
-    app.get('/healthz/ready:', (_req: express.Request,res: express.Response) => {
+    app.get('/healthz/ready', (_req: express.Request,res: express.Response) => {
         res.status(200).send('OK')
     });
 };
--- inspector/tests/health.test.ts
+++ inspector/tests/health.test.ts
@@ -5,7 +5,7 @@ describe('Health check', () => {
     it('responds with 200 OK', (done) => {
         const app = createInspectorServer();
         request(app)
-            .get('/healthz/ready:')
+            .get('/healthz/ready')
             .expect(200,'OK')
             .end((err:Error) => {
                 if (err) throw err;
