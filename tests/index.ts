4366df66be0c9bdc4b2f45d2e72057917a1e6a9a
Jacopo Bufalino <jacopo.bufalino@vaana.fi>
2020-07-13 17:32:45 +0200

    Add inspector deployment

--- tests/index.ts
+++ tests/index.ts
@@ -1,6 +1,7 @@
 import {getDockerCredentialsFromMinikube, getDockerEnv, setupMinikube} from "./minikube";
 const Docker = require('dockerode');
 const path = require('path');
+const K8s = require('k8s');
 
 async function buildInspector(docker) {
     let dockerStream: NodeJS.ReadableStream = await docker.buildImage({
@@ -11,17 +12,25 @@ async function buildInspector(docker) {
         docker.modem.followProgress(dockerStream, (err, res) => err ? reject(err) : resolve(res), (evt) => console.log(evt));
     });
 }
+async function applyInspector(kubectl) {
 
+    return await kubectl.deployment.create('../deployments/inspector.yaml')
+}
 async function main(){
-    const env = await setupMinikube().then(getDockerEnv)
-   // const env = getDockerEnv();
+   // const env = await setupMinikube().then(getDockerEnv)
+    const env = await getDockerEnv();
     const credentials = getDockerCredentialsFromMinikube(env);
     console.log('Logging into docker with:',credentials);
     const docker = new Docker({
         ...credentials
     });
-    await buildInspector(docker)
-
+    await buildInspector(docker);
+    // Create API engine for kubernetes
+    const kubectl = K8s.kubectl({
+        binary: '/usr/local/bin/kubectl'
+        ,version: '/api/v1'
+    });
+    await applyInspector(kubectl);
 }
 
 main().then(r => console.log(r));
