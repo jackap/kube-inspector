0ae9d102cd5260bfe8d54af5986f7ff70509a443
Jacopo Bufalino <jacopo.bufalino@vaana.fi>
2020-07-13 17:09:46 +0200

    Extract building step from main function

--- tests/index.ts
+++ tests/index.ts
@@ -1,29 +1,26 @@
 import {getDockerCredentialsFromMinikube, getDockerEnv, setupMinikube} from "./minikube";
 const Docker = require('dockerode');
 const path = require('path');
-async function main(){
-    const credentials = await setupMinikube().then(getDockerEnv)
-        .then((result) => getDockerCredentialsFromMinikube(result));
 
-    console.log('Logging into docker with: ${credentials}');
-    console.log(credentials);
+async function buildInspector(docker) {
+    let dockerStream: NodeJS.ReadableStream = await docker.buildImage({
+        context: path.resolve(__dirname, '..'),
+    }, {t: 'inspector:1.0.0'});
+
+    return new Promise((resolve, reject) => {
+        docker.modem.followProgress(dockerStream, (err, res) => err ? reject(err) : resolve(res), (evt) => console.log(evt));
+    });
+}
+
+async function main(){
+    const env = await setupMinikube().then(getDockerEnv)
+   // const env = getDockerEnv();
+    const credentials = getDockerCredentialsFromMinikube(env);
+    console.log('Logging into docker with:',credentials);
     const docker = new Docker({
         ...credentials
     });
-
-    docker.listContainers(function (err, containers) {
-        containers.forEach(function (containerInfo) {
-            console.log(containerInfo['Image'])
-        });
-    });
-    let dockerStream: NodeJS.ReadableStream = await docker.buildImage({
-        context: path.resolve(__dirname,'..'),
-    }, {t: 'inspector:1.0.0'});
-
-    return await new Promise((resolve, reject) => {
-        console.log('Following build!');
-        docker.modem.followProgress(dockerStream, (err, res) => err ? reject(err) : resolve(res), (evt) => console.log(evt));
-    });
+    await buildInspector(docker)
 
 }
 
