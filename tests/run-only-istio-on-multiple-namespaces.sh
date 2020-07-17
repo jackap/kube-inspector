5652900890cf5a04a368a52d2867c0b7258e7b30
Jacopo Bufalino <jacopo.bufalino@vaana.fi>
2020-07-17 20:49:50 +0200

    Remove old bash scripts and results

--- tests/run-only-istio-on-multiple-namespaces.sh
+++
@@ -1,8 +0,0 @@
-source ./run-kubernetes-cluster.sh
-
-restartMinikube
-buildAndApplyInspector
-installIstio
-installBookinfoOnOtherNamespace
-sleep 60
-kubectl exec -it $(kubectl get pod -l app=inspector -o jsonpath='{.items[0].metadata.name}') npm run start | tee ../results/result-istio-multiple-namespaces.txt
--- tests/run-only-istio-on-single-namespace.sh
+++
@@ -1,7 +0,0 @@
-source ./run-kubernetes-cluster.sh
-
-restartMinikube
-installIstio
-buildAndApplyInspector
-sleep 60
-kubectl exec -it $(kubectl get pod -l app=inspector -o jsonpath='{.items[0].metadata.name}') npm run start | tee ../results/result-istio-single-namespace.txt
