d667e3a489e54f5dc6b1b8171008dec80749fb32
Jacopo Bufalino <jacopo.bufalino@vaana.fi>
2020-07-17 20:19:27 +0200

    Refactor setup and add helper functions

--- tests/setup.ts
+++ tests/setup.ts
@@ -5,9 +5,33 @@ import {installCalico} from "./calico";
 const Docker = require('dockerode');
 const K8s = require('k8s');
 
+const timeoutHandler = async (kubectl) => {
+    console.info('Not all pods are running!');
+    const pods = await kubectl.pod.list();
+    const statuses = pods.items.map((item) => item.status.phase);
+     return !statuses.every((status: string) => status === "Running");
+};
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+export const waitUntilAllPodsAreRunning = async (kubectl) => {
+    let someAreNotRunning = true;
+    while (someAreNotRunning) {
+        someAreNotRunning = await timeoutHandler(kubectl);
+        await timeout(5000);
+    }
+    return
+}
+
 export async function setupTests(){
-    const env = await setupMinikube().then(getDockerEnv);
-    //const env = await getDockerEnv();
+    let env;
+    try {
+        env = await getDockerEnv();
+    }
+    catch (err) {
+        console.error(err);
+        env = await setupMinikube().then(getDockerEnv);
+    }
     const credentials = getDockerCredentialsFromMinikube(env);
     console.info('Logging into docker using IP ',credentials.host);
     const docker = new Docker({
@@ -23,6 +47,7 @@ export async function setupTests(){
     await installIstio(kubectl).catch((e) => (console.log(e)));
     await installCalico(kubectl).catch((e) => (console.log(e)));
 
+    await waitUntilAllPodsAreRunning(kubectl);
     const inspectorUrl = await getServiceUrl('inspector-service');
     console.info('Inspector url is: ',inspectorUrl.trim())
     return {inspectorUrl: inspectorUrl.trim(),kubectl};
