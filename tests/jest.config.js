22a50e25e57960a39ddc92b340fd26f486f6b586
Jacopo Bufalino <jacopo.bufalino@vaana.fi>
2020-07-17 20:36:28 +0200

    Add jest configuration file

---
+++ tests/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'json', 'node','js'],
+  moduleNameMapper: {
+    // This allows importing modules from the tests/ folder using an absolute path
+    '^tests/(.*)$': '<rootDir>/tests/$1',
+    // This allows importing modules from the src/ folder using an absolute path
+    '^app/(.*)$': '<rootDir>/src/$1',
+  },
+  modulePaths: ['<rootDir>/src'],
+  testRegex: './.*(test|spec)\\.(ts)x?$',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts'],
+};
