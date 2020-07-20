31060fb8785d08567c798f2fd6a053ea8b7d6580
Jacopo Bufalino <jacopo.bufalino@vaana.fi>
2020-07-20 10:12:44 +0200

    Change setup function. Make it more reliable and efficient

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,49 +1,64 @@
 import {getDockerCredentialsFromMinikube, getDockerEnv, getServiceUrl, setupMinikube} from "./minikube";
 import {applyInspector, buildInspector} from "./inspector";
-import {installIstio} from "./istio";
+import {installIstioManifest} from "./istio";
 import {installCalico} from "./calico";
 const Docker = require('dockerode');
 const K8s = require('k8s');
 
-const timeoutHandler = async (kubectl) => {
-    console.info('Not all pods are running!');
+const statusHandler = async (kubectl,status) => {
     const pods = await kubectl.pod.list();
     const statuses = pods.items.map((item) => item.status.phase);
-     return !statuses.every((status: string) => status === "Running");
+     return !statuses.every((curr_status: string) => {
+         return curr_status === status
+
+     });
 };
 function timeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-export const waitUntilAllPodsAreRunning = async (kubectl) => {
+export const waitPodsWithStatus = async (kubectl,status='Running') => {
     let someAreNotRunning = true;
     while (someAreNotRunning) {
-        someAreNotRunning = await timeoutHandler(kubectl);
-        await timeout(5000);
+        someAreNotRunning = await statusHandler(kubectl,status);
+        if (someAreNotRunning) {
+            console.info(`Not all pods have state ${status}!`);
+            await timeout(2000);
+        }
     }
     return
 }
 
 export async function setupTests(){
-    
-    const env = await setupMinikube().then(getDockerEnv);
-    // const env = await getDockerEnv();
-    const credentials = getDockerCredentialsFromMinikube(env);
-    console.info('Logging into docker using IP ',credentials.host);
-    const docker = new Docker({
-        ...credentials
-    });
-    await buildInspector(docker);
-    // Create API engine for kubernetes
+    let env;
     const kubectl = K8s.kubectl({
         binary: '/usr/local/bin/kubectl'
         ,version: '/api/v1'
     });
-    await applyInspector(kubectl).catch((e) => console.log(e));
-    await installIstio(kubectl).catch((e) => (console.log(e)));
-    await installCalico(kubectl).catch((e) => (console.log(e)));
+    try {
+        env = await getDockerEnv();
+        const credentials = getDockerCredentialsFromMinikube(env);
+        console.info('Logging into docker using IP ',credentials.host);
+    }
+    catch (e) {
+        console.info('Starting minikube from scratch');
+        env = await setupMinikube().then(getDockerEnv);
+        const credentials = getDockerCredentialsFromMinikube(env);
+        console.info('Logging into docker using IP ',credentials.host);
+        const docker = new Docker({
+            ...credentials
+        });
+        await buildInspector(docker);
+        await installIstioManifest();
+        await installCalico(kubectl).catch((e) => (console.log(e)));
+    }
 
-    await waitUntilAllPodsAreRunning(kubectl);
+    await waitPodsWithStatus(kubectl);
+    return kubectl;
+}
+
+export async function installInspector(kubectl){
+    await applyInspector(kubectl).catch((e) => console.log(e));
     const inspectorUrl = await getServiceUrl('inspector-service');
     console.info('Inspector url is: ',inspectorUrl.trim())
-    return {inspectorUrl: inspectorUrl.trim(),kubectl};
+    return inspectorUrl
 }
