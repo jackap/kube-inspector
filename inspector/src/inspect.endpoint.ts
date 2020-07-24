7bf4b9bdef57ae95ddf0b83afa15c5183de6249d
Jacopo Bufalino <jacopo.bufalino@vaana.fi>
2020-07-24 10:03:49 +0200

    Set explicit timeout for inspect endpoint

--- inspector/src/inspect.endpoint.ts
+++ inspector/src/inspect.endpoint.ts
@@ -3,7 +3,9 @@ import {getInspectorOutput} from "./inspector";
 // @ts-ignore
 
 export const registerInspectEndpoint = (app: express.Application) => {
-    app.get('/inspect', async (_req: express.Request,res: express.Response) => {
+    app.get('/inspect', async (req: express.Request,res: express.Response) => {
+        res.setTimeout(300
+        * 1000, () => console.log('Timed out') );
         console.info('Inspector endpoint called');
         const data = await getInspectorOutput();
         res.status(200).json(data);
