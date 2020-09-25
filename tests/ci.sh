1896156be3e7c0a70a6300849461a424a556c0f8
Jacopo Bufalino <jacopo.bufalino@vaana.fi>
2020-09-25 20:14:20 +0300

    Set flag in ci script

--- tests/ci.sh
+++ tests/ci.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-set -e
+
+set -x
 minikube start --driver=docker --memory=6500 --cpus=2 --network-plugin=cni 1> /dev/null
+./istio-1.7.2/bin/istioctl manifest install --set profile=demo | true | ./istio-1.7.2/bin/istioctl manifest install --set profile=demo
 eval $(minikube -p minikube docker-env) && docker build -t inspector:1.0.0 .. 1> /dev/null
-./istio-1.7.2/bin/istioctl manifest install --set profile=demo 1> /dev/null
