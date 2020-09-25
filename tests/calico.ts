f14feb3f2884c36fd14f748d15be7da12b6a3e97
Jacopo Bufalino <jacopo.bufalino@vaana.fi>
2020-09-25 11:36:55 +0300

    Update calico installation

--- tests/calico.ts
+++ tests/calico.ts
@@ -1,5 +1,4 @@
 export async function installCalico(kubectl){
-    await kubectl.command('apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/rbac.yaml').catch();
     await kubectl.command('apply -f https://docs.projectcalico.org/manifests/calico.yaml').catch();
 
 
