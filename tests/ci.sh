4c99ca54583625fdfcb33267ad83c22dbccaef89
Jacopo Bufalino <jacopo.bufalino@vaana.fi>
2020-10-12 11:09:57 +0300

    Update script

--- tests/ci.sh
+++ tests/ci.sh
@@ -2,5 +2,5 @@
 
 set -x
 minikube start --driver=docker --cpus=2 --network-plugin=cni 1> /dev/null
-./istio-1.7.2/bin/istioctl manifest install --readiness-timeout=15m --set profile=demo | true | ./istio-1.7.2/bin/istioctl manifest install --set profile=demo
+./istio-1.7.2/bin/istioctl manifest install --readiness-timeout=20m --set profile=demo
 eval $(minikube -p minikube docker-env) && docker build -t inspector:1.0.0 .. 1> /dev/null
